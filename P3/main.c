52971af73e5f67a69d14ce1cb6b809cccf93485e
Kevin <47478959+kecoma1@users.noreply.github.com>
2020-12-06 19:49:05 +0100

    Added 1 to 50000

--- P3/main.c
+++ P3/main.c
@@ -11,65 +11,65 @@ int main(int argc, char** argv) {
     
     srand(time(NULL));
 
-    printf("UNIFORM KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 10.000 | n_times=1\n");
+    printf("UNIFORM KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 50.000 | n_times=1\n");
     ret = generate_search_times(bin_search, uniform_key_generator, SORTED, 
-                                "UNIFORM_KEY_BINARY_SEARCH.log", 1000, 10000, 100, 1);
+                                "UNIFORM_KEY_BINARY_SEARCH.log", 1000, 50000, 100, 1);
     if (ret == ERR) { 
         printf("Error in function generate_search_times UNIFORM KEY GENERATOR - BINARY SEARCH\n");
         exit(-1);
     }
 
-    printf("UNIFORM KEY GENERATOR - LINEAR SEARCH - 1000 --+100--> 10.000 | n_times=1\n");
+    printf("UNIFORM KEY GENERATOR - LINEAR SEARCH - 1000 --+100--> 50.000 | n_times=1\n");
     ret = generate_search_times(lin_search, uniform_key_generator, NOT_SORTED, 
-                                "UNIFORM_KEY_LINEAR_SEARCH.log", 1000, 10000, 100, 1);
+                                "UNIFORM_KEY_LINEAR_SEARCH.log", 1000, 50000, 100, 1);
     if (ret == ERR) { 
         printf("Error in function generate_search_times UNIFORM KEY GENERATOR - BINARY SEARCH\n");
         exit(-1);
     }
 
-    printf("POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH - 1000 --+100--> 10.000 | n_times=1\n");
+    printf("POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH - 1000 --+100--> 50.000 | n_times=1\n");
     ret = generate_search_times(lin_auto_search, potential_key_generator, NOT_SORTED, 
-                                "POTENTIAL_KEY_LIN_AUTO_SEARCH_1.log", 1000, 10000, 100, 1);
+                                "POTENTIAL_KEY_LIN_AUTO_SEARCH_1.log", 1000, 50000, 100, 1);
     if (ret == ERR) { 
         printf("Error in function generate_search_times POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH\n");
         exit(-1);
     }
 
-    printf("POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH - 1000 --+100--> 10.000 | n_times=100\n");
+    printf("POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH - 1000 --+100--> 50.000 | n_times=100\n");
     ret = generate_search_times(lin_auto_search, potential_key_generator, NOT_SORTED, 
-                                "POTENTIAL_KEY_LIN_AUTO_SEARCH_100.log", 1000, 10000, 100, 100);
+                                "POTENTIAL_KEY_LIN_AUTO_SEARCH_100.log", 1000, 50000, 100, 100);
     if (ret == ERR) { 
         printf("Error in function generate_search_times POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH\n");
         exit(-1);
     }
 
-    printf("POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH - 1000 --+100--> 10.000 | n_times=10000\n");
+    printf("POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH - 1000 --+100--> 50.000 | n_times=10000\n");
     ret = generate_search_times(lin_auto_search, potential_key_generator, NOT_SORTED, 
-                                "POTENTIAL_KEY_LIN_AUTO_SEARCH_10000.log", 1000, 10000, 100, 10000);
+                                "POTENTIAL_KEY_LIN_AUTO_SEARCH_10000.log", 1000, 50000, 100, 10000);
     if (ret == ERR) { 
         printf("Error in function generate_search_times POTENTIAL KEY GENERATOR - LINEAR AUTO SEARCH\n");
         exit(-1);
     }
 
-    printf("POTENTIAL KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 10.000 | n_times=1\n");
+    printf("POTENTIAL KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 50.000 | n_times=1\n");
     ret = generate_search_times(bin_search, potential_key_generator, SORTED, 
-                                "POTENTIAL_KEY_BINARY_SEARCH_1.log", 1000, 10000, 100, 1);
+                                "POTENTIAL_KEY_BINARY_SEARCH_1.log", 1000, 50000, 100, 1);
     if (ret == ERR) { 
         printf("Error in function generate_search_times POTENTIAL KEY GENERATOR - BINARY SEARCH\n");
         exit(-1);
     }
 
-    printf("POTENTIAL KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 10.000 | n_times=100\n");
+    printf("POTENTIAL KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 50.000 | n_times=100\n");
     ret = generate_search_times(bin_search, potential_key_generator, SORTED, 
-                                "POTENTIAL_KEY_BINARY_SEARCH_100.log", 1000, 10000, 100, 100);
+                                "POTENTIAL_KEY_BINARY_SEARCH_100.log", 1000, 50000, 100, 100);
     if (ret == ERR) { 
         printf("Error in function generate_search_times POTENTIAL KEY GENERATOR - BINARY SEARCH\n");
         exit(-1);
     }
 
-    printf("POTENTIAL KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 10.000 | n_times=10000\n");
+    printf("POTENTIAL KEY GENERATOR - BINARY SEARCH - 1000 --+100--> 50.000 | n_times=10000\n");
     ret = generate_search_times(bin_search, potential_key_generator, SORTED, 
-                                "POTENTIAL_KEY_BINARY_SEARCH_10000.log", 1000, 10000, 100, 10000);
+                                "POTENTIAL_KEY_BINARY_SEARCH_10000.log", 1000, 50000, 100, 10000);
     if (ret == ERR) { 
         printf("Error in function generate_search_times POTENTIAL KEY GENERATOR - BINARY SEARCH\n");
         exit(-1);
